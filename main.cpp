d1a49756b4b9d85151c0a9e4d274ddc864704948
Mateusz Przybyla <mateusz.przybyla.programista@gmail.com>
2024-03-08 19:41:25 +0100

    Added bubble sort algorithm and sorting time

--- main.cpp
+++ main.cpp
@@ -3,9 +3,29 @@
 
 using namespace std;
 
+void bubbleSort(int numbers, int arr[])
+{
+    int temp = 0;
+
+    for (int i = 1; i < numbers; i++)
+    {
+        for (int j = numbers - 1; j >= i; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                temp = arr[j - 1];
+                arr[j - 1] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
 int main()
 {
     int numbers = 0;
+    clock_t start, stop;
+    double sortingTime = 0;
 
     cout << ">>>> COMPARISON OF SORTING ALGORITHMS <<<<" << endl;
     cout << "==========================================" << endl << endl;
@@ -20,6 +40,15 @@ int main()
     {
         arr1[i] = rand()%100000 + 1;
     }
+
+    cout << endl;
+    cout << "<<BUBBLE SORT>>" << endl;
+    start = clock();
+    bubbleSort(numbers, arr1);
+    stop = clock();
+    sortingTime = (double)(stop - start) / CLOCKS_PER_SEC;
+
+    cout << endl << "Bubble sort time: " << sortingTime << " s" << endl;
 
     delete [] arr1;
 
